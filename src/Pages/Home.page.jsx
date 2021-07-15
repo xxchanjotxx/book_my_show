c9d4cb1bea225ff69f82bd6e7b0ba3877c2f23f9
xxchanjotxx <78687582+xxchanjotxx@users.noreply.github.com>
2021-07-15 23:57:29 +0530

    Entertaiment done

--- src/Pages/Home.page.jsx
+++ src/Pages/Home.page.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
 import EntertainmentCardSlider from "../components/Entertainment/entertaimentCard.component";
+import Premier from "../components/Premier/premier.component";
 
 const HomePage = () => {
   return (
     <>
       <div className="container mx-auto px-4">
-          <h1 className="text-2xl font-bold text-grey-700 my-3"> The best of Entertainment</h1>
+        <h1 className="text-2xl font-bold text-grey-700 my-3">
+          {" "}
+          The best of Entertainment
+        </h1>
         <EntertainmentCardSlider />
       </div>
+      <Premier />
     </>
   );
 };
---
+++ src/components/Poster/poster.component.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Poster = (props) => {
+  return (
+    <>
+      <div className="flex flex-col items-start gap-2 px-3">
+        <div className="h-80">
+          <img
+            src={props.src}
+            alt={props.title}
+            className="w-full h-full rounded-xl"
+          />
+        </div>
+        {/* TEMPELATE LITERAL TO MAKE TEXT WHITE IF BACKGORUND DARK, saying if dark then white else gray 700*/}
+        <h3
+          className={`text-lg font-bold ${
+            props.isDark ? "text-white" : "text-gray-700 "
+          }`}
+        >
+          {props.title}
+        </h3>
+        <p
+          className={`text-sm ${
+            props.isDark ? "text-white" : "text-gray-700 "
+          }`}
+        >
+          {props.subtitle}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Poster;
