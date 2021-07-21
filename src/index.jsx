5d7749eebe158b0750676bc5449f1a282de84d9f
xxchanjotxx <78687582+xxchanjotxx@users.noreply.github.com>
2021-07-21 20:32:55 +0530

    Pagess

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,8 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
+    <BrowserRouter basename={window.location.pathname || ""}>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
