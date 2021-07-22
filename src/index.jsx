d780f3c35584713443fe1b3674ab186eba8677bc
xxchanjotxx <78687582+xxchanjotxx@users.noreply.github.com>
2021-07-22 20:15:26 +0530

    deploying

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="book_my_show">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
